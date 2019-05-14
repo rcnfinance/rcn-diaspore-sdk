345d4af5a05c5a763e2ea07b0e289021757bdaa5
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-05-14 16:44:33 -0300

    [ADD] contract factory with registry contract

--- src/factories/contract_factory.ts
+++ src/factories/contract_factory.ts
@@ -2,15 +2,15 @@ import { TxData, Provider } from 'ethereum-types';
 import {
   LoanManagerContract,
   DebtEngineContract,
-  NanoLoanModelContract,
   InstallmentsModelContract,
+  RegistryContract,
   IERC20Contract
 } from '@jpgonzalezra/abi-wrappers';
 import {
   IERC20,
   LoanManager,
   DebtEngine,
-  NanoLoanModel,
+  Registry,
   InstallmentsModel
 } from '@jpgonzalezra/diaspore-contract-artifacts';
 import { Web3Wrapper } from '@0x/web3-wrapper';
@@ -18,30 +18,42 @@ import { DiasporeContracts, NetworkId } from '../types';
 import assert from '../utils/assert';
 import getDefaultContractAddresses from '../utils/addresses';
 
+async function getRcnRegistryContract(web3Wrapper: Web3Wrapper, address?: string) {
+  return new RegistryContract(
+    Registry.abi,
+    address || (await getDefaultContractAddresses((await web3Wrapper.getNetworkIdAsync()) as NetworkId)), // for optional address
+    web3Wrapper.getProvider(),
+    web3Wrapper.getContractDefaults(),
+  );
+}
+
 export default class ContractFactory {
   private web3Wrapper: Web3Wrapper;
 
   private provider: Provider;
-  private rcnToken: string;
+  private rcnRegistry: Promise<RegistryContract>;
   private contractDefaults: Partial<TxData>;
 
-  public constructor(web3Wrapper: Web3Wrapper, rcnToken: string) {
+  public constructor(web3Wrapper: Web3Wrapper, rcnRegistryAddress: string) {
     this.web3Wrapper = web3Wrapper;
     this.provider = web3Wrapper.getProvider() as Provider;
     this.contractDefaults = this.web3Wrapper.getContractDefaults();
-    this.rcnToken = rcnToken;
+    this.rcnRegistry = getRcnRegistryContract(web3Wrapper, rcnRegistryAddress);;
   }
 
   public async getRcnTokenContract(): Promise<IERC20Contract> {
     return new IERC20Contract(
       IERC20.abi,
-      this.rcnToken,
+      await (await this.rcnRegistry).getAddress.callAsync(DiasporeContracts.IERC20Contract),
       this.provider,
       this.contractDefaults,
     );
   }
 
-  public async getLoanManagerContract(address: string): Promise<LoanManagerContract> {
+  public async getLoanManagerContract(address?: string): Promise<LoanManagerContract> {
+    if (address == undefined) {
+      address = await (await this.rcnRegistry).getAddress.callAsync(DiasporeContracts.DebtEngineContract);
+    }
     assert.isETHAddressHex('address', address);
     return new LoanManagerContract(
       LoanManager.abi,
@@ -51,7 +63,10 @@ export default class ContractFactory {
     );
   }
 
-  public async getDebtEngineContract(address: string): Promise<DebtEngineContract> {
+  public async getDebtEngineContract(address?: string): Promise<DebtEngineContract> {
+    if (address == undefined) {
+      address = await (await this.rcnRegistry).getAddress.callAsync(DiasporeContracts.DebtEngineContract);
+    }
     assert.isETHAddressHex('address', address);
     return new DebtEngineContract(
       DebtEngine.abi,
@@ -61,16 +76,10 @@ export default class ContractFactory {
     );
   }
 
-  public async getNanoLoanModelContract(address: string): Promise<NanoLoanModelContract> {
-    assert.isETHAddressHex('address', address);
-    return new NanoLoanModelContract(
-      NanoLoanModel.abi,
-      address,
-      this.provider,
-      this.contractDefaults,
-    );
-  }
-  public async getInstallmentsModelContract(address: string): Promise<InstallmentsModelContract> {
+  public async getInstallmentsModelContract(address?: string): Promise<InstallmentsModelContract> {
+    if (address == undefined) {
+      address = await (await this.rcnRegistry).getAddress.callAsync(DiasporeContracts.DebtEngineContract);
+    }
     assert.isETHAddressHex('address', address);
     return new InstallmentsModelContract(
       InstallmentsModel.abi,
