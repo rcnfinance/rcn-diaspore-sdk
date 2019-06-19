015290d6ad7b6ff965e49305ec71533ede92d472
Agustin Aguilar <Agusxrun@gmail.com>
2019-06-19 17:44:25 -0300

    Add requestLoan test

--- e2e/marmo/loans.test.ts
+++ e2e/marmo/loans.test.ts
@@ -1,9 +1,10 @@
-
 import { RPCSubprovider, Web3ProviderEngine } from '@0x/subproviders';
+import { Provider, Wallet, DefaultConf } from 'marmojs';
+import { BigNumber } from '@0x/utils';
+import * as crypto from 'crypto';
 import { E2E_CONF } from '../conf';
 import { DiasporeFactory } from '../../src/diaspore_factory';
-import { Provider, Wallet, DefaultConf } from 'marmojs';
-const crypto = require('crypto')
+
 
 test("Request a loan using Marmo", async () => {
     // Create Web3 Provider
@@ -15,13 +16,26 @@ test("Request a loan using Marmo", async () => {
     DefaultConf.ROPSTEN.asDefault();
 
     // Create pk
-    const privateKey = crypto.randomBytes(64).toString('hex');
+    const privateKey = `0x${crypto.randomBytes(32).toString("hex")}`;
+    const wallet = new Wallet(privateKey);
     const diaspore = DiasporeFactory.getMarmoDiasporeAPI(
         E2E_CONF.directory,
         providerEngine,
         new Provider(E2E_CONF.node, E2E_CONF.marmo.relayer),
-        new Wallet(privateKey)
+        wallet
     );
 
-    console.log(new Wallet(privateKey).address);
+    const receipt: string = await diaspore.request({
+        amount: new BigNumber("1"),
+        borrower: wallet.address,
+        salt: new BigNumber("0"),
+        expiration: new BigNumber("900000000"),
+        cuota: new BigNumber("12"),
+        interestRate: new BigNumber("90"),
+        installments: new BigNumber("90"),
+        duration: new BigNumber("86400"),
+        timeUnit: 1
+    });
+
+    expect(receipt).not.toBe("");
 });
